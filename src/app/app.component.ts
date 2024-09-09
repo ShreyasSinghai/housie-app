eda56357ced55a8cc791818ab9323ab26fc07afe
ShreyasSinghai <shreyasyashu1998@gmail.com>
2024-09-09 22:27:34 +0530

    this is added to github

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'housie-app';
+  numbers: number[] = [];
+  usedNumbers: number[] = [];
+  rows: number[][] = [];
+  randomNumber: number | string = 'Not generated yet';
+  isRandomizing: boolean = false;
+  highlightedNumber: number | null = null;
+  highlightedNumbers: Set<number> = new Set();
+  isButtonDisabled: boolean = false;
+  prizeForm = new FormGroup({
+    firstLine: new FormControl(false),
+    secondLine: new FormControl(false),
+    thirdLine: new FormControl(false),
+    corners: new FormControl(false),
+    firstFullHouse: new FormControl(false),
+    secondFullHouse: new FormControl(false),
+    thirdFullHouse: new FormControl(false),
+  });
+
+  ngOnInit() {
+    this.generateNumbers();
+    this.createRows();
+  }
+
+  generateNumbers() {
+    for (let i = 1; i <= 90; i++) {
+      this.numbers.push(i);
+    }
+  }
+
+  createRows() {
+    for (let i = 0; i < this.numbers.length; i += 10) {
+      this.rows.push(this.numbers.slice(i, i + 10));
+    }
+  }
+
+  generateRandomNumber() {
+    this.isButtonDisabled = true;
+    const availableNumbers = this.numbers.filter(
+      (num) => !this.usedNumbers.includes(num)
+    );
+
+    if (availableNumbers.length > 0) {
+      this.isRandomizing = true;
+      const randomizingInterval = setInterval(() => {
+        this.randomNumber = Math.floor(Math.random() * 90) + 1;
+      }, 100);
+
+      setTimeout(() => {
+        clearInterval(randomizingInterval);
+        const randomIndex = Math.floor(Math.random() * availableNumbers.length);
+        this.randomNumber = availableNumbers[randomIndex];
+        this.highlightedNumbers.add(this.randomNumber);
+        this.usedNumbers.push(this.randomNumber);
+        this.isRandomizing = false;
+        this.isButtonDisabled = false;
+      }, 2000);
+    } else {
+      this.randomNumber = 'All numbers used!';
+      this.isButtonDisabled = false;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+//import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatTableModule,
+    MatGridListModule,
+    MatProgressSpinnerModule,
+    MatCheckboxModule,
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
